42f79c4ac6f716cd7d319c46aac420afa87143ec
daniellamarr <danielchidiebele@gmail.com>
2026-06-14 00:23:37 +0100

    fix wrong pulse import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', '~/modules/pulse'],
+  modules: ['@nuxtjs/tailwindcss', '@sycamoreng/pulse-nuxt'],
   css: ['~/assets/css/main.css'],
   app: {
     head: {
